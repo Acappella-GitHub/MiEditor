d29d0b1eacdc69713eb5ea521f6ef9ac31ee187b
fucccccccck <1770622191@qq.com>
2018-05-08 15:37:33 +0800

    Initial commit

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div id="app-vue">
+    <div id="toolbar">
+
+    </div>
+    <div id="sidebar">
+      <hr>
+    </div>
+    <div id="editorbar">
+
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'mieditor'
+    name: 'app-vue'
   }
 </script>
 
 <style>
-  /* CSS */
+  #toolbar {
+    position: absolute;
+    width: 60px;
+    height: 100%;
+    background: #333;
+  }
+
+  #sidebar {
+    position: absolute;
+    top: 0;
+    left: 60px;
+    width: 290px;
+    height: 100%;
+    background: #222;
+  }
+
+  #editorbar {
+    position: absolute;
+    top: 0;
+    left: 350px;
+    right: 0;
+    height: 100%;
+    background: #111;
+  }
+
+  #sidebar hr {
+    position: absolute;
+    right: 0;
+    margin: 0;
+    width: 2px;
+    height: 100%;
+    background: #555;
+    border: none;
+  }
 </style>
